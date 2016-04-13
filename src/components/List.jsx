1579d4fa24787bc494ca26ec0a8e55dbf29ffa83
Daniel Lee <Dan.clee12@gmail.com>
2016-04-12 23:22:25 -0700

    react skeleton

---
+++ src/components/List.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var ListItem = require('./Listitem.jsx');
+
+var ingredients = [{"id":1,"text":"ham"}, {"id":2,"text":"cheese"},{"id":3,"text":"potatoes"}];
+
+module.exports = React.createClass({
+    render: function() {
+        var listItems = ingredients.map(function(item) {
+            return <ListItem key={item.id} ingredient={item.text} />;
+        });
+
+        return (<ul>{listItems}</ul>);
+    }
+});
---
+++ src/components/Listitem.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+module.exports = React.createClass({
+	render: function() {
+		return (
+  			<li>
+  				<h4>{this.props.ingredient}</h4>
+  			</li>
+  			
+		);
+	}
+});
+
